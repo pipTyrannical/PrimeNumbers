a7c2727d1fcf352cc27d50ffe239fd4ea86fc20e
J3ldo <97303347+J3ldo@users.noreply.github.com>
2022-05-19 14:02:52 +0200

    Added a preformance mode.

--- Finder.py
+++ Finder.py
@@ -3,19 +3,34 @@ import time
 primes = [2]
 num = 3
 
-
+preformance = True if input("Enable preformance mode? Y/N: ") == "y" else False
 amountofprimes = int(input("Amount of primes you want to find: "))
 
-q = time.perf_counter()
-while len(primes) < amountofprimes:
-    print("Found: ", len(primes), "primes")
-    for i in primes:
-        if not num / i % 1:
-            break
 
-    else:
-        primes.append(num)
-    num += 2
+if not preformance:
+    q = time.perf_counter()
+    while len(primes) < amountofprimes:
+        print("Found: ", len(primes), "primes")
+        for i in primes:
+            if not num / i % 1:
+                break
+
+        else:
+            primes.append(num)
+        num += 2
+
+else:
+    q = time.perf_counter()
+    print('Preformance mode on. This wont show any messages.')
+    while len(primes) < amountofprimes:
+        for i in primes:
+            if not num / i % 1:
+                break
+
+        else:
+            primes.append(num)
+        num += 2
+
 
 print(primes)
 print(f"Finished in: ", time.perf_counter() - q)
