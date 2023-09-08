0ae7183a5d07dbe5895d041fa89b99687684972b
pipTyrannical <144415863+pipTyrannical@users.noreply.github.com>
2023-09-08 14:30:28 +0600

    MASSIVE performance boost. [18.4x Faster]
    code is now much better but i feel i got carried away and changed too much.

--- Finder.py
+++ Finder.py
@@ -1,36 +1,29 @@
 import time
 
-primes = [2]
-num = 3
 
-preformance = True if input("Enable preformance mode? Y/N: ") == "y" else False
+def find_prime_numbers(amount_of_primes):
+    primes = [2]
+    num = 3
+
+    while len(primes) < amount_of_primes:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+
+                break
+
+        if is_prime:
+            primes.append(num)
+
+        num += 2
+    return primes
+
+
 amountofprimes = int(input("Amount of primes you want to find: "))
 
-
-if not preformance:
-    q = time.perf_counter()
-    while len(primes) < amountofprimes:
-        print("Found: ", len(primes), "primes")
-        for i in primes:
-            if not num % i:
-                break
-
-        else:
-            primes.append(num)
-        num += 2
-
-else:
-    q = time.perf_counter()
-    print('Preformance mode on. This wont show any messages.')
-    while len(primes) < amountofprimes:
-        for i in primes:
-            if not num % i:
-                break
-
-        else:
-            primes.append(num)
-        num += 2
-
-
-print(primes)
-print(f"Finished in: ", time.perf_counter() - q)
+start_time = time.perf_counter()
+prime_list = find_prime_numbers(amountofprimes)
+end_time = time.perf_counter()
+print(f"Found {len(prime_list)} prime numbers in {end_time - start_time:.5f} seconds.")
+print(prime_list)
